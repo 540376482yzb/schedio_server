a8a96bfaf837ae1736fcf03bfca13d7012157f2e
Zhou <540376482yzb@gmail.com>
2018-05-08 22:36:19 -0700

    lastest update

--- routes/widgetroutes/display.widget.routes.js
+++ routes/widgetroutes/display.widget.routes.js
@@ -7,7 +7,7 @@ const Event = require('../../models/events.models');
 router.put('/:id/display', (req, res, next) => {
   const { widgets } = req.body;
   const { id } = req.params;
-  return Event.findByIdAndUpdate(id, { $set: { widgets } }, { new: true }).then(res => {
+  return Event.findByIdAndUpdate(id, { $set: { widgets } }, { new: true }).then(event => {
     /*=======todo: testing for update display
     */
     res.status(200).json(event);
